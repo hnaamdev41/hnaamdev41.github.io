388b209207f7cd6c3eb544a69a3eef96cbf59158
hnaamdev41 <hnaamdev41@users.noreply.github.com>
2025-01-15 12:10:47 +0000

    deploy: 21265c34d7048ff247b2aa946dae714b05a61d78

---
+++ precache-manifest.ddf24ed0cc158da5551ea5797e8749ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf87bc373da13aee7efae41dd4fff32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d055ae1351edbbccd32a",
+    "url": "/static/css/main.23e81eea.chunk.css"
+  },
+  {
+    "revision": "6103b256937633577112",
+    "url": "/static/js/2.7628f88d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.7628f88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d055ae1351edbbccd32a",
+    "url": "/static/js/main.9666fa27.chunk.js"
+  },
+  {
+    "revision": "35a581bbf7f86f6cbda9",
+    "url": "/static/js/runtime-main.cb1e2c17.js"
+  }
+]);
