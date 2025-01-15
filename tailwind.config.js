078556974451438f9fdc59947a01bb00c0d047c8
Himanshu Naamdev <hnaamdev41@gmail.com>
2025-01-15 18:02:33 +0530

    Fix CSS and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./public/index.html"
   ],
   darkMode: 'class',
   theme: {
@@ -23,42 +23,10 @@ module.exports = {
         '1/4': '25%',
         '1/2': '50%',
         '3/4': '75%',
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#9ca3af',
-            a: {
-              color: '#60a5fa',
-              '&:hover': {
-                color: '#93c5fd',
-              },
-            },
-            h1: {
-              color: '#f3f4f6',
-            },
-            h2: {
-              color: '#f3f4f6',
-            },
-            h3: {
-              color: '#f3f4f6',
-            },
-            h4: {
-              color: '#f3f4f6',
-            },
-            code: {
-              color: '#e5e7eb',
-              backgroundColor: '#374151',
-              padding: '0.25rem',
-              borderRadius: '0.25rem',
-              fontWeight: '400',
-            },
-          },
-        },
-      },
+      }
     },
   },
   plugins: [
     require('@tailwindcss/typography'),
   ],
-};
+}
